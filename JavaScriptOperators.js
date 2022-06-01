e9374ae081ac7bec9dc8ad5c667661feba07a5e2
Jacob Crowe <jacobcrowe@Jacobs-MacBook-Pro.local>
2022-06-01 13:51:17 -0500

    Init commit

---
+++ JavaScriptOperators.js
@@ -0,0 +1,46 @@
+// Declare 2 variables, a and b, and assign 20 to a and 4 to b
+// Declare a variable add that uses the + operator to store the result of adding the values stored in a and b
+// Declare a variable minus that uses the - operator to store the result of subtracting the values stored in a and b
+// Declare a variable multiply that uses the * operator to store the result of multiplying the values stored in a and b
+// Declare a variable dividing that uses the / operator to store the result of dividing the values stored in a and b
+
+//let a = 20; //Commented out to continue exercise
+let b = 4;
+let add = 20 + 4;
+let minus = 20 - 4;
+let multiply = 20 * 4;
+let divide = 20 / 4;
+
+//Use the following code to answer the questions below:
+
+let a = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+
+// What is the value of: a + str? answer is "1111"
+// What is the value of: a + str2? answer is "11eleven"
+// What is the value of: a + isPresent? answer is 12
+// What is the value of: a + firstName? answer is "11Jackie"
+// What is the value of: a + lastName? answer is "11Chan"
+
+//Use the following code to answer the questions below:
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+// What is the value of: d == str3? true
+// What is the value of: d === str3? false
+// What is the value of: !isPresent2? syntax error
+// What is the value of: (“eleven” == str4 && d >= str3)? false
+// What is the value of: (!isPresent2 || isPresent2)? syntax error
+// What is the value of: 0 == false? true
+// What is the value of: 0 === false? true
+// What is the value of: 0 != false? false
+// What is the value of 0 !== false? syntax error
+
